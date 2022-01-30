f91538206a2696329edae55eaaf91a3572eea8ae
ABDULRAHMAN NOFAL <92503455+Abdul-Nofal@users.noreply.github.com>
2022-01-30 07:19:39 +0000

    Added Colors and made auto shading system.

--- public/script.js
+++ public/script.js
@@ -27,10 +27,10 @@ class Wall {
     this.sx = sx;
     this.sy = sy;
     this.options = options;
-    this.material = new THREE.MeshBasicMaterial({
-      color: options.color || 0xbd4b48,
-      //map: options.map || checker
-    });
+    if(!options.color) {
+      options.color = new Color("#e5e5e5");
+    }
+    this.material = new THREE.MeshBasicMaterial();
     this.geometry = new THREE.BoxGeometry(sx, sy, options.clip ? 0.07 : 0.08);
     this.mesh = new THREE.Mesh(this.geometry, this.material);
     this.mesh.position.x = x;
@@ -48,6 +48,16 @@ class Wall {
     if(options.clip) {
       this.mesh.clip = options.clip;
     }
+    
+    this.direction = new THREE.Vector3();
+    this.mesh.getWorldDirection(this.direction);
+    if(this.direction.x === Math.max(this.direction.x, this.direction.y, this.direction.z)) {
+      this.mesh.material.color = new THREE.Color(options.color.light || 0x00ff00);
+    }else if(this.direction.y === Math.max(this.direction.x, this.direction.y, this.direction.z)) {
+      this.mesh.material.color = new THREE.Color(options.color.dark || 0x00ff00);      
+    }else {
+      this.mesh.material.color = new THREE.Color(options.color.hex || 0x00ff00);
+    }
     scene.add(this.mesh);
     blocks.push(this.mesh);
   }
@@ -61,15 +71,16 @@ class Floor {
     this.sx = sx;
     this.sy = sy;
     this.options = options;
+    if(!options.color) {
+      options.color = new Color("#e5e5e5");
+    }
+
     this.texture = new THREE.TextureLoader().load(options.map || "/assets/textures/floor.jpg");
     this.texture.wrapS = THREE.RepeatWrapping;
     this.texture.wrapT = THREE.RepeatWrapping;
     this.texture.repeat.set(sx/3.5, sy/3.5);
 
-    this.material = new THREE.MeshBasicMaterial({
-      color: options.color || 0xbd4b48,
-      //map: this.texture,
-      side: THREE.DoubleSide
+    this.material = new THREE.MeshBasicMaterial({         side: THREE.DoubleSide
     });
     this.geometry = new THREE.PlaneGeometry(sx, sy);
     this.mesh = new THREE.Mesh(this.geometry, this.material);
@@ -88,9 +99,33 @@ class Floor {
     if(options.clip) {
       this.mesh.clip = options.clip;
     }
+
+    this.direction = new THREE.Vector3();
+    this.mesh.getWorldDirection(this.direction);
+    if(this.direction.x === Math.max(this.direction.x, this.direction.y, this.direction.z)) {
+      this.mesh.material.color = new THREE.Color(options.color.light || 0x00ff00);
+    }else if(this.direction.y === Math.max(this.direction.x, this.direction.y, this.direction.z)) {
+      this.mesh.material.color = new THREE.Color(options.color.dark || 0x00ff00);      
+    }else {
+      this.mesh.material.color = new THREE.Color(options.color.hex || 0x00ff00);
+    }
     scene.add(this.mesh);
   }
 }
+
+class Color {
+  constructor(hex, intensity=15) {
+    this.hex = hex;
+    this.dark = this.adjust(hex, -intensity);
+    this.light = this.adjust(hex, intensity);
+  }
+  adjust(color, amount) {
+    return '#' + color.replace(/^#/, '').replace(/../g, color => ('0'+Math.min(255, Math.max(0, parseInt(color, 16) + amount)).toString(16)).substr(-2));
+  }
+}
+
+let blue = new Color("#50B2C0");
+let beigh = new Color("#FEEFDD");
 
 // Start
 function Start() {
@@ -170,29 +205,31 @@ function Movement() {
 function BuildHouse() {
   // Master Bedroom
   new Wall(0, 0, 0, 35, 4, {
-    color: 0xaa4441
+    color: blue
   });
   new Wall(3.5*5, 0, 3.5*3, 3.5*6, 4, {
     rotation: {
       y: 90
-    }
+    },
+    color: blue
   });
   new Wall(-3.5*5, 0, 3.5*3, 3.5*6, 4, {
     rotation: {
       y: 90
-    }
+    },
+    color: blue
   });
   new Wall(-3.5*2.875, 0, 3.5*6, 3.5*4.25, 4, {
-    color: 0xaa4441
+    color: blue
   });
   new Wall(3.5*2.875, 0, 3.5*6, 3.5*4.25, 4, {
-    color: 0xaa4441
+    color: blue
   });
   new Floor(0, 0, 3.5*3, 10*3.5, 6*3.5, {
-    color: 0x973c3a
+    color: beigh
   });
   new Floor(0, 4, 3.5*3, 10*3.5, 6*3.5, {
-    color: 0x973c3a
+    color: blue
   });
 
   
